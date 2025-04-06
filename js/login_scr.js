c01bd6fd9f185306ed3649b8db56df4ad64cc382
Gowreesh V.T <vt.gowreesh43@gmail.com>
2025-04-06 18:31:40 +0530

    123

--- js/login_scr.js
+++ js/login_scr.js
@@ -4,22 +4,20 @@ const registerLink = document.querySelector(".register-link1");
 const btnPopup = document.querySelector(".btnLogin-popup");
 const closeIcon = document.querySelector(".icon-close");
 
-registerLink.addEventListener("click", ()=> {
-    wrapper.classList.add("active");
-    wrapper.classList.remove("active-slide");
-})
-
-loginLink.addEventListener("click", ()=> {
-    wrapper.classList.remove("active");
-    wrapper.classList.add("active-slide");
-})
-
-btnPopup.addEventListener("click", ()=> {
-    wrapper.classList.add("active-popup");
-})
-
-closeIcon.addEventListener("click", () => {
-    wrapper.classList.remove("active-popup");
+registerLink.addEventListener("click", () => {
+  wrapper.classList.add("active");
+  wrapper.classList.remove("active-slide");
 });
 
+loginLink.addEventListener("click", () => {
+  wrapper.classList.remove("active");
+  wrapper.classList.add("active-slide");
+});
 
+btnPopup.addEventListener("click", () => {
+  wrapper.classList.add("active-popup");
+});
+
+closeIcon.addEventListener("click", () => {
+  wrapper.classList.remove("active-popup");
+});
